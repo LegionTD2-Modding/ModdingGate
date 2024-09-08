d17602c360d36f8903ae856449336eb4150ab58e
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2024-09-08 22:04:58 +0200

    Rewrite json parser

--- ModsGate.cs
+++ ModsGate.cs
@@ -35,267 +35,281 @@ using HarmonyLib;
 using Newtonsoft.Json;
 using System.IO.Compression;
 
-namespace ModsGate
+namespace ModsGate;
+
+using C = Constants;
+using Dependency = Dictionary<string, string>;
+
+[BepInPlugin("org.kidev.ltd2.modsgate", "Mods Gate", "1.0.0")]
+public class ModsGate : BaseUnityPlugin
 {
-    using C = Constants;
-    using Dependency = Dictionary<string, string>;
+    private readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+    private readonly Harmony _harmony = new("org.kidev.ltd2.modsgate");
 
-    [BepInPlugin("org.kidev.ltd2.modsgate", "Mods Gate", "1.0.0")]
-    public class ModsGate : BaseUnityPlugin
+    private static readonly HttpClient Client = new HttpClient();
+    private static ManualLogSource _logger;
+    private string _gatewayFileAbs;
+    private string _gatewayFileModdedAbs;
+    private string _htmlUrl;
+    private int _htmlInjectionLine;
+
+    public void Awake()
     {
-        private readonly Assembly _assembly = Assembly.GetExecutingAssembly();
-        private readonly Harmony _harmony = new("org.kidev.ltd2.modsgate");
-        
-        private static readonly HttpClient Client = new HttpClient();
-        private static ManualLogSource _logger;
-        private string _gatewayFileAbs;
-        private string _gatewayFileModdedAbs;
-        private string _htmlUrl;
-        private int _htmlInjectionLine;
-        
-        public void Awake()
-        {
-            _logger = Logger;
-            _logger.LogInfo("Mods gate loaded!");
-            
-            _gatewayFileAbs =
-                Path.Combine(Paths.GameRootPath, "Legion TD 2_Data", "uiresources", "AeonGT", C.GatewayFileName);
-            _gatewayFileModdedAbs =
-                Path.Combine(Paths.GameRootPath, "Legion TD 2_Data", "uiresources", "AeonGT", C.GatewayFileNameModded);
-            
-            try {
-                RemoveTempFiles();
-                _harmony.PatchAll(_assembly);
-            }
-            catch (Exception e) {
-                Logger.LogError($"Error while patching: {e}");
-                throw;
-            }
+        _logger = Logger;
+        _logger.LogInfo("Mods gate loaded!");
 
-            _ = CheckForUpdatesAsync();
-        }
-        
-        public void OnDestroy() {
+        _gatewayFileAbs =
+            Path.Combine(Paths.GameRootPath, "Legion TD 2_Data", "uiresources", "AeonGT", C.GatewayFileName);
+        _gatewayFileModdedAbs =
+            Path.Combine(Paths.GameRootPath, "Legion TD 2_Data", "uiresources", "AeonGT", C.GatewayFileNameModded);
+
+        try
+        {
             RemoveTempFiles();
+            _harmony.PatchAll(_assembly);
         }
-        
-        private void InjectIntoGateway()
+        catch (Exception e)
         {
-            var lines = File.ReadAllLines(_gatewayFileAbs);
-            using (var client = new System.Net.WebClient())
-            {
-                string htmlContent = client.DownloadString(_htmlUrl);
-                lines[_htmlInjectionLine] = htmlContent + Environment.NewLine + lines[_htmlInjectionLine];
-            }
-
-            File.WriteAllLines(_gatewayFileModdedAbs, lines);
+            Logger.LogError($"Error while patching: {e}");
+            throw;
         }
 
-        private void RemoveTempFiles() {
-            if (File.Exists(_gatewayFileModdedAbs)) {
-                File.Delete(_gatewayFileModdedAbs);
-            }
-        }
-        
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        [SuppressMessage("ReSharper", "UnusedMember.Local")]
-        [HarmonyPatch]
-        internal static class PatchSendCreateView
-        {
-            private static Type _typeCoherentUIGTView;
-            
-            [HarmonyPrepare]
-            private static void Prepare() {
-                _typeCoherentUIGTView = AccessTools.TypeByName("CoherentUIGTView");
-            }
-            
-            [HarmonyTargetMethod]
-            private static MethodBase TargetMethod() {
-                return AccessTools.Method(_typeCoherentUIGTView, "SendCreateView");
-            }
-            
-            [HarmonyPrefix]
-            private static bool SendCreateViewPre(ref string ___m_Page) {
-                if (___m_Page.Equals(C.GatewayFile)) {
-                    ___m_Page = C.GatewayFileModded;
-                }
-                return true;
-            }
-        }
-
-        private async Task CheckForUpdatesAsync()
-        {
-            try
-            {
-                string jsonContent = await FetchFileContentAsync(C.JsonURL);
-                ModData modData = JsonConvert.DeserializeObject<ModData>(jsonContent);
-                List<PluginInfo> installedPlugins = GetInstalledPlugins();
-                _htmlUrl = modData.Core.InjectHtml;
-                _htmlInjectionLine = modData.Core.InjectLine;
-                
-                InjectIntoGateway();
-
-                _ = modData.Mods.Prepend(
-                    new Mod(modData.Core.Name, 
-                        modData.Core.Author, 
-                        modData.Core.IconUrl,
-                        modData.Core.Url,
-                        modData.Core.Version,
-                        modData.Core.GameVersion,
-                        modData.Core.Description
-                    )
-                );
-
-                foreach (var mod in modData.Mods)
-                {
-                    var installedPlugin = installedPlugins.FirstOrDefault(p => p.Metadata.Name == mod.Name);
-                    if (installedPlugin == null) continue;
-                    Version installedVersion = new Version(installedPlugin.Metadata.Version.ToString());
-                    Version jsonVersion = new Version(mod.Version);
-
-                    mod.ReplaceVersionInUrls();
-
-                    if (jsonVersion <= installedVersion) continue;
-                    _logger.LogInfo($"Update available for {mod.Name}: {installedVersion} -> {jsonVersion}");
-                    await UpdateModAsync(mod, installedPlugin);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error during update check: {ex.Message}");
-            }
-        }
-
-        private static async Task<string> FetchFileContentAsync(string url)
-        {
-            HttpResponseMessage response = await Client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
-        }
-
-        private static List<PluginInfo> GetInstalledPlugins()
-        {
-            return BepInEx.Bootstrap.Chainloader.PluginInfos.Values.ToList();
-        }
-
-        private static async Task UpdateModAsync(Mod mod, PluginInfo installedPlugin)
-        {
-            try
-            {
-                string downloadUrl = mod.GetUrlForOS();
-                string zipPath = Path.Combine(Paths.PluginPath, $"{mod.Name}_update.zip");
-                string extractPath = Path.Combine(Paths.PluginPath, mod.Name);
-
-                _logger.LogInfo($"Downloading {downloadUrl} to {extractPath}");
-                using (var response = await Client.GetAsync(downloadUrl))
-                using (var fs = new FileStream(zipPath, FileMode.CreateNew))
-                {
-                    await response.Content.CopyToAsync(fs);
-                }
-
-                string backupPath = Path.Combine(Paths.PluginPath,
-                    $"{mod.Name}_v{installedPlugin.Metadata.Version}.outdated");
-                Directory.Move(extractPath, backupPath);
-
-                ZipFile.ExtractToDirectory(zipPath, extractPath);
-
-                File.Delete(zipPath);
-
-                _logger.LogInfo($"Successfully updated {mod.Name} to version {mod.Version}");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error updating {mod.Name}: {ex.Message}");
-            }
-        }
+        _ = CheckForUpdatesAsync();
     }
 
-    [SuppressMessage("ReSharper", "CollectionNeverUpdated.Global")]
-    public class ModData
+    public void OnDestroy()
     {
-        public Core Core { get; set; }
-        public List<Mod> Mods { get; set; }
+        RemoveTempFiles();
     }
 
-    public class Core
+    private void InjectIntoGateway()
     {
-        public string Name { get; set; }
-        public string Author { get; set; }
-        public string IconUrl { get; set; }
-        public Dependency Url { get; set; }
-        public string Version { get; set; }
-        public string GameVersion { get; set; }
-        public string Description { get; set; }
-        public string InjectHtml { get; set; }
-        public int InjectLine { get; set; }
-        public List<Dependency> Dependencies { get; set; }
-        public List<Dependency> DependenciesVersions { get; set; }
-        public List<Dependency> Installers { get; set; }
-        public string Signatures { get; set; }
-    }
-
-    public class Mod(
-        string modName,
-        string author,
-        string iconUrl,
-        Dependency url,
-        string version,
-        string gameVersion,
-        string description)
-    {
-        public string Name { get; set; } = modName;
-        public string Author { get; set; } = author;
-        public string IconUrl { get; set; } = iconUrl;
-        public Dependency Url { get; set; } = url;
-        public string Version { get; set; } = version;
-        public string GameVersion { get; set; } = gameVersion;
-        public string Description { get; set; } = description;
-
-        public string GetUrlForOS()
+        var lines = File.ReadAllLines(_gatewayFileAbs);
+        using (var client = new System.Net.WebClient())
         {
-            if (Url.TryGetValue("*", out var fromMap))
-            {
-                return fromMap;
-            }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Url.TryGetValue("win", out var valueFromMap))
-            {
-                return valueFromMap;
-            }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Url.TryGetValue("linux", out var map1))
-            {
-                return map1;
-            }
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && Url.TryGetValue("mac", out var fromMap1))
-            {
-                return fromMap1;
-            }
-
-            throw new Exception($"Unsupported OS: {RuntimeInformation.OSDescription}");
+            string htmlContent = client.DownloadString(_htmlUrl);
+            lines[_htmlInjectionLine] = htmlContent + Environment.NewLine + lines[_htmlInjectionLine];
         }
 
-        public void ReplaceVersionInUrls()
-        {
-            if (Url == null || Version == null)
-                return;
+        File.WriteAllLines(_gatewayFileModdedAbs, lines);
+    }
 
-            var keys = new List<string>(Url.Keys); // To avoid modifying the dictionary while iterating
-            foreach (var key in keys)
-            {
-                // Replace all occurrences of '$' with the value of Version
-                Url[key] = Url[key].Replace("$", Version);
-            }
+    private void RemoveTempFiles()
+    {
+        if (File.Exists(_gatewayFileModdedAbs))
+        {
+            File.Delete(_gatewayFileModdedAbs);
         }
     }
-    
-    internal static class Constants
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [HarmonyPatch]
+    internal static class PatchSendCreateView
     {
-        internal const string GatewayFileName = "gateway.html";
-        internal const string GatewayFileNameModded = "__gateway.html";
-        internal const string GatewayFile = "coui://uiresources/AeonGT/gateway.html";
-        internal const string GatewayFileModded = "coui://uiresources/AeonGT/__gateway.html";
-        internal const string JsonURL = "https://raw.githubusercontent.com/LegionTD2-Modding/.github/main/mods/config.json";
+        private static Type _typeCoherentUIGTView;
+
+        [HarmonyPrepare]
+        private static void Prepare()
+        {
+            _typeCoherentUIGTView = AccessTools.TypeByName("CoherentUIGTView");
+        }
+
+        [HarmonyTargetMethod]
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(_typeCoherentUIGTView, "SendCreateView");
+        }
+
+        [HarmonyPrefix]
+        private static bool SendCreateViewPre(ref string ___m_Page)
+        {
+            if (___m_Page.Equals(C.GatewayFile))
+            {
+                ___m_Page = C.GatewayFileModded;
+            }
+
+            return true;
+        }
     }
+
+    private async Task CheckForUpdatesAsync()
+    {
+        try
+        {
+            string jsonContent = await FetchFileContentAsync(C.JsonURL);
+            ModData modData = JsonConvert.DeserializeObject<ModData>(jsonContent);
+            List<PluginInfo> installedPlugins = GetInstalledPlugins();
+            _htmlUrl = modData.Core.InjectHtml;
+            _htmlInjectionLine = modData.Core.InjectLine;
+
+            InjectIntoGateway();
+
+            _ = modData.Mods.Prepend(
+                new Mod(modData.Core.Name,
+                    modData.Core.Author,
+                    modData.Core.IconUrl,
+                    modData.Core.Url,
+                    modData.Core.Version,
+                    modData.Core.GameVersion,
+                    modData.Core.Description
+                )
+            );
+
+            foreach (var mod in modData.Mods)
+            {
+                var installedPlugin = installedPlugins.FirstOrDefault(p => p.Metadata.Name == mod.Name);
+                if (installedPlugin == null) continue;
+                Version installedVersion = new Version(installedPlugin.Metadata.Version.ToString());
+                Version jsonVersion = new Version(mod.Version);
+
+                mod.ReplaceVersionInUrls();
+
+                if (jsonVersion <= installedVersion) continue;
+                _logger.LogInfo($"Update available for {mod.Name}: {installedVersion} -> {jsonVersion}");
+                await UpdateModAsync(mod, installedPlugin);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error during update check: {ex.Message}");
+        }
+    }
+
+    private static async Task<string> FetchFileContentAsync(string url)
+    {
+        HttpResponseMessage response = await Client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    private static List<PluginInfo> GetInstalledPlugins()
+    {
+        return BepInEx.Bootstrap.Chainloader.PluginInfos.Values.ToList();
+    }
+
+    private static async Task UpdateModAsync(Mod mod, PluginInfo installedPlugin)
+    {
+        try
+        {
+            string downloadUrl = mod.GetUrlForOS();
+            string zipPath = Path.Combine(Paths.PluginPath, $"{mod.Name}_update.zip");
+            string extractPath = Path.Combine(Paths.PluginPath, mod.Name);
+
+            _logger.LogInfo($"Downloading {downloadUrl} to {extractPath}");
+            using (var response = await Client.GetAsync(downloadUrl))
+            using (var fs = new FileStream(zipPath, FileMode.CreateNew))
+            {
+                await response.Content.CopyToAsync(fs);
+            }
+
+            string backupPath = Path.Combine(Paths.PluginPath,
+                $"{mod.Name}_v{installedPlugin.Metadata.Version}.outdated");
+            Directory.Move(extractPath, backupPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            File.Delete(zipPath);
+
+            _logger.LogInfo($"Successfully updated {mod.Name} to version {mod.Version}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error updating {mod.Name}: {ex.Message}");
+        }
+    }
+}
+
+public class Mod(
+    string name,
+    string author,
+    string iconUrl,
+    Dictionary<string, string> url,
+    string version,
+    string gameVersion,
+    string description)
+{
+    [JsonProperty("name")] public string Name { get; set; } = name;
+    [JsonProperty("author")] public string Author { get; set; } = author;
+    [JsonProperty("icon_url")] public string IconUrl { get; set; } = iconUrl;
+    [JsonProperty("url")] public Dictionary<string, string> Url { get; set; } = url;
+    [JsonProperty("version")] public string Version { get; set; } = version;
+    [JsonProperty("game_version")] public string GameVersion { get; set; } = gameVersion;
+    [JsonProperty("description")] public string Description { get; set; } = description;
+
+    public string GetUrlForOS()
+    {
+        if (Url.TryGetValue("*", out var fromMap))
+        {
+            return fromMap;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Url.TryGetValue("win", out var valueFromMap))
+        {
+            return valueFromMap;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Url.TryGetValue("linux", out var map1))
+        {
+            return map1;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && Url.TryGetValue("mac", out var fromMap1))
+        {
+            return fromMap1;
+        }
+
+        throw new Exception($"Unsupported OS: {RuntimeInformation.OSDescription}");
+    }
+
+    public void ReplaceVersionInUrls()
+    {
+        if (Url == null || Version == null)
+            return;
+
+        var keys = new List<string>(Url.Keys); // To avoid modifying the dictionary while iterating
+        foreach (var key in keys)
+        {
+            // Replace all occurrences of '$' with the value of Version
+            Url[key] = Url[key].Replace("$", Version);
+        }
+    }
+}
+
+public class Core
+{
+    [JsonProperty("name")] public string Name { get; set; }
+    [JsonProperty("author")] public string Author { get; set; }
+    [JsonProperty("icon_url")] public string IconUrl { get; set; }
+    [JsonProperty("url")] public Dictionary<string, string> Url { get; set; }
+    [JsonProperty("version")] public string Version { get; set; }
+    [JsonProperty("game_version")] public string GameVersion { get; set; }
+    [JsonProperty("description")] public string Description { get; set; }
+    [JsonProperty("inject_html")] public string InjectHtml { get; set; }
+    [JsonProperty("inject_line")] public int InjectLine { get; set; }
+    [JsonProperty("dependancies")] public List<Dictionary<string, string>> Dependencies { get; set; }
+    [JsonProperty("dependancies_versions")] public List<DependencyVersion> DependencyVersions { get; set; }
+    [JsonProperty("installers")] public Dictionary<string, string> Installers { get; set; }
+    [JsonProperty("signatures")] public string Signatures { get; set; }
+}
+
+public class DependencyVersion
+{
+    [JsonProperty("name")] public string Name { get; set; }
+    [JsonProperty("version")] public string Version { get; set; }
+}
+
+public class ModData
+{
+    [JsonProperty("core")] public Core Core { get; set; }
+    [JsonProperty("mods")] public List<Mod> Mods { get; set; }
+}
+
+internal static class Constants
+{
+    internal const string GatewayFileName = "gateway.html";
+    internal const string GatewayFileNameModded = "__gateway.html";
+    internal const string GatewayFile = "coui://uiresources/AeonGT/gateway.html";
+    internal const string GatewayFileModded = "coui://uiresources/AeonGT/__gateway.html";
+    internal const string JsonURL = "https://raw.githubusercontent.com/LegionTD2-Modding/.github/main/mods/config.json";
 }
